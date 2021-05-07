d4caaa9dfc0b87126de01550149e532e178dbcc9
Okonkwo Emmanuel <jahsminemma@gmail.com>
2021-05-07 14:25:12 +0100

    Update StateProvider.js

--- Desktop/zuri-chatApp/chatapp/src/StateProvider.js
+++ Desktop/zuri-chatApp/chatapp/src/StateProvider.js
@@ -2,7 +2,7 @@ import { useReducer, useContext, createContext } from "react"
 
 const stateContext = createContext()
 
-export const StateProvider = ({ reducer, InitialState, children }) => (
+export const StateProvider = ({ reducer, children }) => (
     <stateContext.Provider value={useReducer(reducer, InitialState)}>
         {children}
     </stateContext.Provider>
